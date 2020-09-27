10d78ded432da52cd8328b2e0204439a372842b8
minamikawatakuya <55489051+minamikawatakuya@users.noreply.github.com>
2020-09-27 22:54:36 +0900

    cell作成まで

---
+++ realmTest/placeCell.swift
@@ -0,0 +1,24 @@
+//
+//  placeCell.swift
+//  realmTest
+//
+//  Created by minamikawa on 2020/09/27.
+//  Copyright © 2020 minamikawa. All rights reserved.
+//
+
+import UIKit
+
+class placeCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
