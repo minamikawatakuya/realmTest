732f835ceaf4c6b2a708bc85e4059a4cb51856a0
minamikawatakuya <55489051+minamikawatakuya@users.noreply.github.com>
2020-09-27 22:41:15 +0900

    view3作成

---
+++ realmTest/View3Controller.swift
@@ -0,0 +1,30 @@
+//
+//  View3Controller.swift
+//  realmTest
+//
+//  Created by minamikawa on 2020/09/27.
+//  Copyright © 2020 minamikawa. All rights reserved.
+//
+
+import UIKit
+
+class View3Controller: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
