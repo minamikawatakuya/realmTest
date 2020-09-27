2ebb80fe033d2bae224e3dab0cd683110837546b
minamikawatakuya <55489051+minamikawatakuya@users.noreply.github.com>
2020-09-27 22:46:38 +0900

    項目設置

--- realmTest/Place.swift
+++ realmTest/Place.swift
@@ -4,6 +4,12 @@ import Foundation
  
 class Place: Object {
  
-    @objc dynamic var memo: String? = nil
+    dynamic var id: Int? = nil
+    dynamic var name: String? = nil
+    dynamic var address: String? = nil
+    dynamic var latitude: Double? = nil
+    dynamic var longitude: Double? = nil
+    dynamic var identifier: String? = nil
+    dynamic var note: String? = nil
         
 }
