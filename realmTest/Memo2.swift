bd63b40390ec5387a0bfb43a0bd1f84210e01f47
minamikawatakuya <55489051+minamikawatakuya@users.noreply.github.com>
2020-09-28 18:21:53 +0900

    realmへの記録、成功

---
+++ realmTest/Memo2.swift
@@ -0,0 +1,10 @@
+import UIKit
+import RealmSwift
+import Foundation
+ 
+class Memo2: Object {
+ 
+    @objc dynamic var name: String? = nil
+    @objc dynamic var address: String? = nil
+        
+}
--- realmTest/View4Controller.swift
+++ realmTest/View4Controller.swift
@@ -17,19 +17,48 @@ class View4Controller: UIViewController, UITableViewDelegate, UITableViewDataSou
     @IBOutlet weak var table: UITableView!
     
     // モデルクラスを使用し、取得データを格納する変数を作成
-    var tableCells: Results<Memo>!
+    var tableCells: Results<Memo2>!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Interface Builderのファイルを読み込む
+        let nib = UINib(nibName: "placeCell", bundle: nil)
+        
+        // UITableViewに登録する。NewsCellを使用するという宣言
+        table.register(nib, forCellReuseIdentifier: "placeCell")
 
         // Realmインスタンス取得
         let realm = try! Realm()
          
         // データ全権取得
-        self.tableCells = realm.objects(Memo.self)
+        self.tableCells = realm.objects(Memo2.self)
     }
     
     @IBAction func pushButton(_ sender: Any) {
+        
+        // モデルクラスのインスタンスを取得
+        let MemoInstance:Memo2 = Memo2()
+         
+        // テキスト入力値をインスタンスに詰める
+        //MemoInstance.memo = self.textField.text
+        MemoInstance.name = self.nameField.text
+        MemoInstance.address = self.addressField.text
+         
+        // Realmインスタンス取得
+        let realm = try! Realm()
+         
+        // DB登録処理
+        try! realm.write {
+            realm.add(MemoInstance)
+        }
+         
+        // テーブル再読み込み
+        self.table.reloadData()
+        
+        self.nameField.text = "";
+        self.addressField.text = "";
+        
     }
     
     // cellの数を指定
@@ -40,13 +69,24 @@ class View4Controller: UIViewController, UITableViewDelegate, UITableViewDataSou
     // cellに値を設定
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 
-        let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-
-        let tmpCell: Memo = self.tableCells[(indexPath as NSIndexPath).row];
-
-        cell.textLabel?.text = tmpCell.memo
-
-        return cell;
+        //let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        //let tmpCell: Memo = self.tableCells[(indexPath as NSIndexPath).row];
+        //cell.textLabel?.text = tmpCell.memo
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "placeCell",
+        for: indexPath) as! placeCell
+        
+        let tmpCell: Memo2 = self.tableCells[(indexPath as NSIndexPath).row];
+        
+        cell.nameLabel.text = tmpCell.name
+        cell.addressLabel.text = tmpCell.address
+        
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
     }
 
 }
